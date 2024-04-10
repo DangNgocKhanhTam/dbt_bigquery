a76068d83627dcbb5b14e2fc68b76ddf556a7968
tamdang <tamdangngoc0707@gmail.com>
2024-04-11 06:43:46 +0700

    add key in dim customer

--- models/example/analytics/dim_customer.sql
+++ models/example/analytics/dim_customer.sql
@@ -7,17 +7,23 @@ WITH
   dim_customer__rename_column AS (
   SELECT
     customer_id AS customer_key,
-    customer_name
+    customer_name, 
+    customer_category_id as customer_category_key, 
+    buying_group_id as buying_group_key
   FROM
     dim_customer__source ),
   dim_customer__cast_type AS (
   SELECT
     CAST(customer_key AS integer) AS customer_key,
-    CAST(customer_name AS string) AS customer_name
+    CAST(customer_name AS string) AS customer_name, 
+    CAST(customer_category_key as integer) as customer_category_key, 
+    CAST(buying_group_key as integer) as buying_group_key
   FROM
     dim_customer__rename_column )
 SELECT
   customer_key,
-  customer_name
+  customer_name, 
+  customer_category_key, 
+  buying_group_key
 FROM
   dim_customer__cast_type
