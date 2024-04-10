013f0c8857bb97b9e4610ae6064577759926d36e
tamdang <tamdangngoc0707@gmail.com>
2024-04-10 14:13:53 +0700

    dim product

--- models/example/analytics/product.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-  stock_item_id as product_id, 
-  coalesce(brand, 'Undefined') as brand_name, 
-  product.supplier_id, 
-  supplier.supplier_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items` as product 
-JOIN {{ ref('supplier') }} as supplier
-ON product.supplier_id = supplier.supplier_id
--- models/example/analytics/supplier.sql
+++
@@ -1,4 +0,0 @@
-SELECT 
-supplier_id,
-upper(supplier_name) as supplier_name
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers` 
