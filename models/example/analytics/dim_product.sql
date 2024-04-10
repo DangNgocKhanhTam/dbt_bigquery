87b0a7f1c2fa2cf8d9bfe202f1e75ae71fb3e709
tamdang <tamdangngoc0707@gmail.com>
2024-04-11 06:26:47 +0700

    ref _dim_supplier

--- models/example/analytics/dim_product.sql
+++ models/example/analytics/dim_product.sql
@@ -8,19 +8,24 @@ WITH
   SELECT
     stock_item_id AS product_key,
     stock_item_name AS product_name,
-    brand AS brand_name
+    brand AS brand_name,
+    supplier_id as supplier_key 
   FROM
     dim_product__source ),
   dim_product_cast_type AS (
   SELECT
     CAST(product_key AS integer) AS product_key,
     CAST(product_name AS string) AS product_name,
-    CAST(brand_name AS string) AS brand_name
+    CAST(brand_name AS string) AS brand_name, 
+    CAST(supplier_key as integer) as supplier_key
   FROM
     dim_product__rename_column )
 SELECT
-  product_key,
-  product_name,
-  brand_name
-FROM
-  dim_product_cast_type
+  dim_product.product_key,
+  dim_product.product_name,
+  dim_product.brand_name, 
+  dim_product.supplier_key, 
+  dim_supplier.supplier_name
+FROM  dim_product_cast_type as dim_product
+LEFT JOIN {{ref('dim_supplier')}} as dim_supplier
+ON dim_product.supplier_key = dim_supplier.supplier_key
