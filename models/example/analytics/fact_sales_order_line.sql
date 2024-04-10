4fee431781339329b5150f26d4247319a8974c4c
tamdang <tamdangngoc0707@gmail.com>
2024-04-10 19:14:17 +0700

    cast_type_fact_sales_order_line

--- models/example/analytics/fact_sales_order_line.sql
+++ models/example/analytics/fact_sales_order_line.sql
@@ -1,7 +1,19 @@
+with fact_sales_order_line__source as (
+  select * 
+  from `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+, fact_sales_order_line__rename_column as (
+  select 
+  order_line_id as sales_order_line_key, 
+  stock_item_id as product_key,
+  quantity,
+  unit_price
+  from fact_sales_order_line__source
+)
 SELECT 
-  cast(order_line_id as integer)  as sales_order_line_key, 
-  cast(stock_item_id as integer) as product_key,
+  cast(sales_order_line_key as integer)  as sales_order_line_key, 
+  cast(product_key as integer) as product_key,
   cast(quantity as integer) as quantity, 
   cast(unit_price as numeric) as unit_price , 
   cast(quantity as integer) * cast(unit_price as numeric) as gross_amount
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+FROM fact_sales_order_line__rename_column
