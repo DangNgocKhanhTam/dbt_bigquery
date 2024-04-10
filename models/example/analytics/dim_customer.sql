b2b0f77943d17fd47351045bb42cd778a778f97d
tamdang <tamdangngoc0707@gmail.com>
2024-04-10 19:38:05 +0700

    add_key_fact_sales_order_lines

---
+++ models/example/analytics/dim_customer.sql
@@ -0,0 +1,23 @@
+WITH
+  dim_customer__source AS (
+  SELECT
+    *
+  FROM
+    `vit-lam-data.wide_world_importers.sales__customers` ),
+  dim_customer__rename_column AS (
+  SELECT
+    customer_id AS customer_key,
+    customer_name
+  FROM
+    dim_customer__source ),
+  dim_customer__cast_type AS (
+  SELECT
+    CAST(customer_key AS integer) AS customer_key,
+    CAST(customer_name AS string) AS customer_name
+  FROM
+    dim_customer__rename_column )
+SELECT
+  customer_key,
+  customer_name
+FROM
+  dim_customer__cast_type
--- models/example/analytics/fact_sales_order_line.sql
+++ models/example/analytics/fact_sales_order_line.sql
@@ -4,16 +4,27 @@ with fact_sales_order_line__source as (
 )
 , fact_sales_order_line__rename_column as (
   select 
-  order_line_id as sales_order_line_key, 
+  order_line_id as sales_order_line_key,
+  order_id as sales_order_key, 
   stock_item_id as product_key,
   quantity,
   unit_price
   from fact_sales_order_line__source
-)
+), fact_sales_order_line__cast_type as (
 SELECT 
   cast(sales_order_line_key as integer)  as sales_order_line_key, 
+  cast(sales_order_key as integer) as sales_order_key,
   cast(product_key as integer) as product_key,
   cast(quantity as integer) as quantity, 
   cast(unit_price as numeric) as unit_price , 
   cast(quantity as integer) * cast(unit_price as numeric) as gross_amount
 FROM fact_sales_order_line__rename_column
+) 
+select 
+sales_order_line_key,
+sales_order_key,
+product_key, 
+quantity,
+unit_price, 
+gross_amount 
+from fact_sales_order_line__cast_type
