6ec636c170e02a4a576c90901403584fae061518
tamdang <tamdangngoc0707@gmail.com>
2024-04-10 14:06:59 +0700

    add gross amount

--- models/example/analytics/product.sql
+++ models/example/analytics/product.sql
@@ -4,5 +4,5 @@ SELECT
   product.supplier_id, 
   supplier.supplier_name
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items` as product 
-JOIN `learn-dbt2.test2.supplier` as supplier
+JOIN {{ ref('supplier') }} as supplier
 ON product.supplier_id = supplier.supplier_id
