c5aea5a41b1fe2a322f3678bcf3d2c9e44d03084
tamdang <tamdangngoc0707@gmail.com>
2024-04-10 19:46:48 +0700

    create model stg_fact_sales_order

--- models/example/analytics/fact_sales_order_line.sql
+++ models/example/analytics/fact_sales_order_line.sql
@@ -1,30 +1,34 @@
-with fact_sales_order_line__source as (
-  select * 
-  from `vit-lam-data.wide_world_importers.sales__order_lines`
-)
-, fact_sales_order_line__rename_column as (
-  select 
-  order_line_id as sales_order_line_key,
-  order_id as sales_order_key, 
-  stock_item_id as product_key,
+WITH
+  fact_sales_order_line__source AS (
+  SELECT
+    *
+  FROM
+    `vit-lam-data.wide_world_importers.sales__order_lines` ),
+  fact_sales_order_line__rename_column AS (
+  SELECT
+    order_line_id AS sales_order_line_key,
+    order_id AS sales_order_key,
+    stock_item_id AS product_key,
+    quantity,
+    unit_price
+  FROM
+    fact_sales_order_line__source ),
+  fact_sales_order_line__cast_type AS (
+  SELECT
+    CAST(sales_order_line_key AS integer) AS sales_order_line_key,
+    CAST(sales_order_key AS integer) AS sales_order_key,
+    CAST(product_key AS integer) AS product_key,
+    CAST(quantity AS integer) AS quantity,
+    CAST(unit_price AS numeric) AS unit_price,
+    CAST(quantity AS integer) * CAST(unit_price AS numeric) AS gross_amount
+  FROM
+    fact_sales_order_line__rename_column )
+SELECT
+  sales_order_line_key,
+  sales_order_key,
+  product_key,
   quantity,
-  unit_price
-  from fact_sales_order_line__source
-), fact_sales_order_line__cast_type as (
-SELECT 
-  cast(sales_order_line_key as integer)  as sales_order_line_key, 
-  cast(sales_order_key as integer) as sales_order_key,
-  cast(product_key as integer) as product_key,
-  cast(quantity as integer) as quantity, 
-  cast(unit_price as numeric) as unit_price , 
-  cast(quantity as integer) * cast(unit_price as numeric) as gross_amount
-FROM fact_sales_order_line__rename_column
-) 
-select 
-sales_order_line_key,
-sales_order_key,
-product_key, 
-quantity,
-unit_price, 
-gross_amount 
-from fact_sales_order_line__cast_type
+  unit_price,
+  gross_amount
+FROM
+  fact_sales_order_line__cast_type
