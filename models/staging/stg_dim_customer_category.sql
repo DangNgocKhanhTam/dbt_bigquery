af91e6a610329bf7271cd19173ef7bbaf3689f2e
tamdang <tamdangngoc0707@gmail.com>
2024-04-11 06:34:14 +0700

    create stg_dim_customer_category

---
+++ models/staging/stg_dim_customer_category.sql
@@ -0,0 +1,19 @@
+with dim_customer_category__source as (
+    SELECT *
+    FROM `vit-lam-data.wide_world_importers.sales__customer_categories`
+)
+, dim_customer_category__rename_column as (
+    select 
+    customer_category_id as customer_category_key,
+    customer_category_name
+    from dim_customer_category__source
+), dim_customer_category__cast_type as (
+    select 
+    CAST(customer_category_key as integer) customer_category_key, 
+    CAST(customer_category_name as string ) customer_category_name
+    from dim_customer_category__rename_column
+)
+select 
+    customer_category_key, 
+    customer_category_name
+from dim_customer_category__cast_type
