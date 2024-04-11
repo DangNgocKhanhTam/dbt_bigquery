e9e0cafe87166b07b66d276634be6663b9936332
tamdang <tamdangngoc0707@gmail.com>
2024-04-11 08:30:45 +0700

    add is credit hold

--- models/example/analytics/dim_customer.sql
+++ models/example/analytics/dim_customer.sql
@@ -8,6 +8,7 @@ WITH
   SELECT
     customer_id AS customer_key,
     customer_name, 
+    is_on_credit_hold as is_on_credit_hold_boolean,
     customer_category_id as customer_category_key, 
     buying_group_id as buying_group_key
   FROM
@@ -16,18 +17,29 @@ WITH
   SELECT
     CAST(customer_key AS integer) AS customer_key,
     CAST(customer_name AS string) AS customer_name, 
+    CAST(is_on_credit_hold_boolean as boolean) is_on_credit_hold_boolean,
     CAST(customer_category_key as integer) as customer_category_key, 
     CAST(buying_group_key as integer) as buying_group_key
   FROM
-    dim_customer__rename_column )
+    dim_customer__rename_column ), 
+  dim_customer__convert_booolean as (
+    select 
+    *, 
+    CASE WHEN is_on_credit_hold_boolean is TRUE THEN 'On Creadit Hold' 
+         WHEN is_on_credit_hold_boolean is FALSE THEN 'Not On Creadit Hold'
+         WHEN is_on_credit_hold_boolean is NULL THEN 'Undefined'
+         ELSE 'Invalid' END is_on_credit_hold
+    from dim_customer__cast_type
+  )
 SELECT
   dim_customer.customer_key,
   dim_customer.customer_name, 
   dim_customer.customer_category_key, 
   dim_customer_category.customer_category_name,
   dim_customer.buying_group_key, 
+  dim_customer.is_on_credit_hold,
   dim_buying_group.buying_group_name
-FROM   dim_customer__cast_type dim_customer
+FROM   dim_customer__convert_booolean dim_customer
 LEFT JOIN {{ ref ('stg_dim_customer_category') }} as dim_customer_category 
 ON dim_customer.customer_category_key = dim_customer_category.customer_category_key
 LEFT JOIN {{ ref ('stg_dim_buying_group') }} as dim_buying_group
