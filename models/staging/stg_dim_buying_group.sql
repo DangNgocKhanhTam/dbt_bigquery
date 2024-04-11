922f5e46d1257835d42e3fdb68845949fd920881
tamdang <tamdangngoc0707@gmail.com>
2024-04-11 11:20:01 +0700

    processing

--- models/staging/stg_dim_buying_group.sql
+++ models/staging/stg_dim_buying_group.sql
@@ -12,8 +12,21 @@ with dim_buying_group__source as (
     CAST(buying_group_key as integer) buying_group_key, 
     CAST(buying_group_name as string ) buying_group_name
     from dim_buying_group__rename_column
+), dim_buying_group__add_undefined_record as (
+    SELECT
+    buying_group_key,
+    buying_group_name
+    FROM dim_buying_group__cast_type
+    UNION ALL
+    SELECT
+    0 as buying_group_key,
+    'Undefined' buying_group_name
+    UNION ALL
+    SELECT
+    -1 as buying_group_key,
+    'Invalid' buying_group_name
 )
 select 
     buying_group_key, 
     buying_group_name
-from dim_buying_group__cast_type
+from dim_buying_group__add_undefined_record
--- models/staging/stg_dim_customer_category.sql
+++ models/staging/stg_dim_customer_category.sql
@@ -12,8 +12,21 @@ with dim_customer_category__source as (
     CAST(customer_category_key as integer) customer_category_key, 
     CAST(customer_category_name as string ) customer_category_name
     from dim_customer_category__rename_column
+), dim_customer_category__add_undefined_record as (
+    SELECT
+    customer_category_key,
+    customer_category_name
+    FROM dim_customer_category__cast_type
+    UNION ALL
+    SELECT
+    0 as customer_category_key,
+    'Undefined' customer_category_name
+    UNION ALL
+    SELECT
+    -1 as customer_category_key,
+    'Invalid' customer_category_name
 )
 select 
     customer_category_key, 
     customer_category_name
-from dim_customer_category__cast_type
+from dim_customer_category__add_undefined_record
